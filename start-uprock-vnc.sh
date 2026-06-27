2db4f1078d93bf5819e7a2c4d298abc75e7d2448
kavie87 <kavie_87@hotmail.com>
2026-06-27 21:05:40 +1000

    Update start-uprock-vnc.sh

--- start-uprock-vnc.sh
+++ start-uprock-vnc.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 set -e
 
-x11vnc \
-  -display :99 \
-  -forever \
-  -shared \
-  -nopw \
-  -rfbport 5900 \
-  -noxdamage &
+echo "Stopping UpRock service and old VNC sessions..."
+systemctl stop uprock || true
+pkill -f uprock-mining || true
+pkill -f x11vnc || true
+pkill -f websockify || true
+pkill -f "Xvfb :98" || true
+rm -f /tmp/.X98-lock
 
-websockify \
-  --web=/usr/share/novnc \
-  6080 \
-  localhost:5900 &
+echo "Starting temporary display :98..."
+Xvfb :98 -screen 0 1280x720x24 -nolisten tcp &
+sleep 3
 
+echo "Starting UpRock GUI on :98..."
+DISPLAY=:98 uprock-mining &
+
+echo "Starting VNC/noVNC..."
+x11vnc -display :98 -forever -shared -nopw -rfbport 5900 -noxdamage &
+websockify --web=/usr/share/novnc 6080 localhost:5900 &
+
+IP=$(hostname -I | awk '{print $1}')
+
+echo
 echo "Open:"
-echo "http://DEVICE-IP:6080/vnc.html"
+echo "http://$IP:6080/vnc.html"
+echo
+echo "When finished, run:"
+echo "./stop-uprock-vnc.sh"
